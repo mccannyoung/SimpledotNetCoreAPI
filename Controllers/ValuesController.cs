9d1cdada2d67038265fda5553ac8c51b2c63b19b
Rebecca McCann-Young <Rebecca.McCann-Young@target.com>
2018-04-13 22:40:28 -0500

    Initial commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpledotNetCoreAPI.Controllers
+{
+    [Route("/")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello World!";
+        }
+        [HttpGet("ping")]
+        public string Get(int id)
+        {
+            return "pong";
+        }
+    }
+}
